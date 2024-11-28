1817b81be0fdebe588c97b5be6dd405fb0de6f6a
Aradhya Shukla <aradhya1703@gmail.com>
2024-11-28 16:12:24 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from django.shortcuts import redirect
 from . import views
 
-# Redirect root URL to external URL
-def root_redirect(request):
-    return redirect('https://dashpro.azurewebsites.net/', permanent=True)  # Permanent redirect to the new URL
-
 urlpatterns = [
-    path('', root_redirect),  # Redirect '/' to the external URL
     path('country_intensity_datas/', views.country_intensity_datas, name='countryintensitydatas'),
     path('datas/', views.datas, name='datas'),
     path('relevance_pestle_datas/', views.relevance_pestle_datas, name='relevancepestledatas'),
-    path('reports', views.reports, name='dashboard-reports'),
-    path('datatables', views.datatabels, name='dashboard-datatables'),
-    path('profile', views.profile, name='dashboard-profile'),
+    path('', views.base, name='base'),
+    path("reports", views.reports, name="dashboard-reports"),
+    path("datatables", views.datatabels, name="dashboard-datatables"),
+    path('', views.base, name='base'),
+    path("reports", views.reports, name="dashboard-reports"),
+    path("datatables", views.datatabels, name="dashboard-datatables"),
+    path("profile", views.profile, name="dashboard-profile"),
+    
 ]
