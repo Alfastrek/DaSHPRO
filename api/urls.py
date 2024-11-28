fa30c8d2e71a0fe7cf0792edd858803a1f93fcfd
Aradhya Shukla <aradhya1703@gmail.com>
2024-11-28 16:28:32 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # API endpoints
     path('country_intensity_datas/', views.country_intensity_datas, name='countryintensitydatas'),
     path('datas/', views.datas, name='datas'),
     path('relevance_pestle_datas/', views.relevance_pestle_datas, name='relevancepestledatas'),
+
+    # Dashboard pages
     path('', views.base, name='base'),
     path("reports", views.reports, name="dashboard-reports"),
-    path("datatables", views.datatabels, name="dashboard-datatables"),
-    path('', views.base, name='base'),
-    path("reports", views.reports, name="dashboard-reports"),
-    path("datatables", views.datatabels, name="dashboard-datatables"),
+    path("datatables", views.datatables, name="dashboard-datatables"),
     path("profile", views.profile, name="dashboard-profile"),
-    
 ]
